1bffeb9a994726cc86e613e3b87b7302ee5a47c3
HannaKobelska <anna.kobelskaya@gmail.com>
2022-11-08 16:47:00 +0200

    hw05_for_check

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,34 @@
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp")
+
+const { User } = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    const extension = originalname.split(".").pop();
+    const filename = `${_id}.${extension}`;
+    
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+
+     Jimp.read(resultUpload, async (error, avatar) => {
+          if (error) throw error;
+           await avatar
+                .resize(256, 256)
+                .write(resultUpload);
+      });
+
+
+    const avatarURL = path.join("public", "avatars", filename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+    
+    res.status(200).json({
+        avatarURL,
+    })
+}
+
+module.exports = updateAvatar;
--- models/user.js
+++ models/user.js
@@ -29,7 +29,11 @@ const userSchema = new Schema({
         type: String,
         enum: ["starter", "pro", "business"],
         default: "starter"
-     },
+    },
+    avatarURL: {
+        type: String,
+        required: true,
+    },
 }, {versionKey: false, timestamps: true});
 
 userSchema.post("save", handleSaveErrors);
